5ce3ce1cdcdb0c989c4cd923d73f320fb1e60aed
harshpatel0000 <harshpketanbhai@gmail.com>
2026-07-16 15:12:35 +0530

    Update index.php

--- hostel/index.php
+++ hostel/index.php
@@ -118,15 +118,7 @@ return true;
 		</div>
 	</div>
 </div>
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
+
 </body>
 
 </html>
