434c9b25b0c7c7cc32e18c34b9b6065ad0150e88
harshpatel0000 <harshpketanbhai@gmail.com>
2026-07-16 17:33:33 +0530

    Completed project

---
+++ hostel/home.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+    <title>Home page</title>
+    <!-- <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <div class="banner1">
+        <img src="img/HMS_IMG.jpeg">
+        <div class="text-box">
+            <header>
+                <!-- <div class="logo">
+                    <img src="logo-removebg-preview.png" alt="error">
+                </div> -->
+                <div class="bg">
+                    <nav class="navbar">
+
+                        <ul>
+                            <!-- <li><a href="home.php">Home</a></li> -->
+                            <!-- <li><a href="service.php">Services</a></li> -->
+                            <!-- <li><a href="about.php">About</a></li> -->
+                            <!-- <li><a href="contact.php">Contact us</a></li> -->
+                            <li><a href="index.php">Login</a></li>
+                            <li><a href="registration.php">Sign up</a></li>
+                        </ul>
+                    </nav>
+
+                    <div class="row">
+                        <div class="col-22">
+                            <form class="form">
+                            <h1 class="h1">R.C.Technical Hostel</h1>
+                            <div class="line">
+                                <hr style="color:black;background-color:black">
+                            </div>
+                            <p>We’ll make you feel right at home.
+<br>The best place to stay while you’re away from home.
+
+                            </p>
+                            <div class="bt btn">
+                                <a href="aboutus.php" class="btn">About Us</a>
+                            </div>
+</form>
+                        </div>
+                        <div class="col-22">
+                            <!-- <img src="Service.png"> -->
+                        </div>
+                    </div>
+            </header>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
